b0c7924387fb74c109bb1a5ce01d18cbfb2e7d77
ICESAGE1 <166036148+ICESAGE1@users.noreply.github.com>
2024-08-23 14:56:12 +0530

    Update Product.js

--- models/Product.js
+++ models/Product.js
@@ -1,1 +1,17 @@
+const mongoose = require('mongoose');
+
+const productSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  image: String,
+  price: {
+    type: Number,
+    required: true,
+  },
+  category: String,
+});
+
+module.exports = mongoose.model('Product', productSchema);
 
