e54555d4bc1c7eebeb4401fa351c14736d551f15
ICESAGE1 <166036148+ICESAGE1@users.noreply.github.com>
2024-08-23 14:49:27 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,1 +1,22 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import CartPage from './pages/CartPage';
+import CheckoutPage from './pages/CheckoutPage';
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/cart" component={CartPage} />
+        <Route path="/checkout" component={CheckoutPage} />
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
 
