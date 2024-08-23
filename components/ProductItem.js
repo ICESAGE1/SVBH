4bf96e54de8df2c8068c4504bd4b16da52dfdc61
ICESAGE1 <166036148+ICESAGE1@users.noreply.github.com>
2024-08-23 14:43:02 +0530

    Update ProductItem.js

--- components/ProductItem.js
+++ components/ProductItem.js
@@ -1,15 +1,19 @@
 import React from 'react';
-import ProductItem from './ProductItem';
 
-function ProductList({ products }) {
+function ProductItem({ product }) {
+  const addToCart = () => {
+    // Logic to add item to cart
+  };
+
   return (
-    <div className="product-list">
-      {products.map(product => (
-        <ProductItem key={product.id} product={product} />
-      ))}
+    <div className="product-item">
+      <img src={product.image} alt={product.name} />
+      <h3>{product.name}</h3>
+      <p>{product.price}</p>
+      <button onClick={addToCart}>Add to Cart</button>
     </div>
   );
 }
 
-export default ProductList;
+export default ProductItem;
 
