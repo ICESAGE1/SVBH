5a0da32107a12dc48e702f53737a159c80bcb706
ICESAGE1 <166036148+ICESAGE1@users.noreply.github.com>
2024-08-23 15:20:05 +0530

    Update Navbar.js

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,1 +1,24 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function Navbar() {
+  return (
+    <nav>
+      <h1>Food & Grocery Store</h1>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/cart">Cart</Link>
+        </li>
+        <li>
+          <Link to="/checkout">Checkout</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
 
