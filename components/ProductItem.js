7e2a3ef00c9e614edbf102e0c723c68eb3e03232
ICESAGE1 <166036148+ICESAGE1@users.noreply.github.com>
2024-08-23 14:41:59 +0530

    Update ProductItem.js

--- components/ProductItem.js
+++ components/ProductItem.js
@@ -1,1 +1,15 @@
+import React from 'react';
+import ProductItem from './ProductItem';
+
+function ProductList({ products }) {
+  return (
+    <div className="product-list">
+      {products.map(product => (
+        <ProductItem key={product.id} product={product} />
+      ))}
+    </div>
+  );
+}
+
+export default ProductList;
 
