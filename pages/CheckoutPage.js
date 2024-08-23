39aaf06810de77296e061e76126016d9f6ca66a0
ICESAGE1 <166036148+ICESAGE1@users.noreply.github.com>
2024-08-23 15:07:13 +0530

    Update CheckoutPage.js

--- pages/CheckoutPage.js
+++ pages/CheckoutPage.js
@@ -1,1 +1,29 @@
+import React from 'react';
+import { useHistory } from 'react-router-dom';
+
+function CheckoutPage() {
+  const history = useHistory();
+
+  const handleCheckout = async () => {
+    const response = await fetch('/api/checkout', {
+      method: 'POST',
+    });
+
+    if (response.ok) {
+      alert('Checkout successful!');
+      history.push('/');
+    } else {
+      alert('Checkout failed!');
+    }
+  };
+
+  return (
+    <div>
+      <h1>Checkout</h1>
+      <button onClick={handleCheckout}>Confirm and Pay</button>
+    </div>
+  );
+}
+
+export default CheckoutPage;
 
