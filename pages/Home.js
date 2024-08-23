66a11d863284efaa962a31a1c08cdf48ad6a0ac2
ICESAGE1 <166036148+ICESAGE1@users.noreply.github.com>
2024-08-23 14:52:07 +0530

    Update Home.js

--- pages/Home.js
+++ pages/Home.js
@@ -1,1 +1,23 @@
+import React, { useEffect, useState } from 'react';
+import ProductList from '../components/ProductList';
+
+function Home() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetch('/api/products')
+      .then(res => res.json())
+      .then(data => setProducts(data))
+      .catch(err => console.error(err));
+  }, []);
+
+  return (
+    <div>
+      <h1>Order Food & Groceries</h1>
+      <ProductList products={products} />
+    </div>
+  );
+}
+
+export default Home;
 
